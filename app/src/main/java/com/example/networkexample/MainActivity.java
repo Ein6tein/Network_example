07dc381b010a640256e259935eb1f53162be5c18
Ein6tein <Ein6tein2@gmail.com>
2020-02-15 13:05:17 +0200

    Display loaded stuff

--- app/src/main/java/com/example/networkexample/MainActivity.java
+++ app/src/main/java/com/example/networkexample/MainActivity.java
@@ -2,20 +2,47 @@ package com.example.networkexample;
 
 import android.os.Bundle;
 
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.networkexample.adapter.EmployeeAdapter;
+import com.example.networkexample.viewmodel.MyViewModel;
 import com.example.networkexample.viewmodel.MyViewModelFactory;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import dagger.android.AndroidInjection;
 import dagger.android.support.DaggerAppCompatActivity;
 
 public class MainActivity extends DaggerAppCompatActivity {
 
+    @BindView(R.id.recycler_view) RecyclerView mRecyclerView;
+
     @Inject MyViewModelFactory mViewModelFactory;
+
+    private EmployeeAdapter mAdapter;
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         AndroidInjection.inject(this);
+    }
+
+    @Override protected void onStart() {
+        super.onStart();
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
+        mRecyclerView.setAdapter(mAdapter = new EmployeeAdapter(this));
+
+        MyViewModel viewModel = new ViewModelProvider(this, mViewModelFactory)
+                .get(MyViewModel.class);
+
+        viewModel.employees().observe(this, mAdapter::setEmployees);
+
+        viewModel.getEmployees();
     }
 }
--- app/src/main/java/com/example/networkexample/viewmodel/MyViewModel.java
+++ app/src/main/java/com/example/networkexample/viewmodel/MyViewModel.java
@@ -1,14 +1,28 @@
 package com.example.networkexample.viewmodel;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.networkexample.model.Employee;
 import com.example.networkexample.network.EmployeeRepository;
+
+import java.util.List;
 
 public class MyViewModel extends ViewModel {
 
     private EmployeeRepository mRepository;
+    private MutableLiveData<List<Employee>> mEmployeesLiveData = new MutableLiveData<>();
 
     public MyViewModel(EmployeeRepository repository) {
         mRepository = repository;
+    }
+
+    public LiveData<List<Employee>> employees() {
+        return mEmployeesLiveData;
+    }
+
+    public void getEmployees() {
+        mRepository.getEmployees().subscribe(employees -> mEmployeesLiveData.postValue(employees));
     }
 }
